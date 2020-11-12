2765b6e2c639c017304f1c0e166ad1786276db66
Aleksey Startcev <startcev.aleksey@gmail.com>
2020-11-12 18:01:00 +0700

    bug fix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "feature.cpp"
 
 double my_pow(double number, unsigned int degree) {
     double numb = number;
@@ -8,11 +9,49 @@ double my_pow(double number, unsigned int degree) {
     return number;
 }
 
+double fast_pow(double num, int deg) {
+    double result = 1;
+    if(deg < 0) {
+        deg = -deg;
+
+        while(deg) {
+            if (deg % 2 == 0) {
+                deg /= 2;
+                num *= num;
+            }
+            else {
+                deg--;
+                result *= num;
+            }
+        }
+
+        return 1 / result;
+    }
+    else {
+        while(deg) {
+            if (deg % 2 == 0) {
+                deg /= 2;
+                num *= num;
+            }
+            else {
+                deg--;
+                result *= num;
+            }
+        }
+
+        return result;
+    }
+}
+
 int main() {
 
-    double number = my_pow(5.15, 3);;
+    double number = my_pow(5.15, 3);
 
-    std::cout << number;
+    std::cout << number << std::endl;
+
+    number = fast_pow(5.15, -3);
+
+    std::cout << number << std::endl;
 
     return 0;
 }
